da9ec5911a19f24b5e85c12e994c74f43c8c1fd9
lawkelvin33 <lawkelvin33@korea.ac.kr>
2023-09-24 04:15:26 -0400

    deleted unnecessary code

--- src/thegraph/base.graph.ts
+++ src/thegraph/base.graph.ts
@@ -1,6 +1,5 @@
 import axios from "axios";
 
-// 가장 최근에 Sync된 blocknumber 구하기
 export const syncBlockQuery = async (subgraph: string) => {
   const query = `{ 
       _meta{
--- src/uniswapV3Positions.ts
+++
@@ -1,6 +0,0 @@
-import { JsonRpcProvider, formatUnits, Interface, formatEther } from "ethers";
-import {
-  PositionManager__factory,
-  UniswapV3Toaster__factory,
-} from "./typechain";
-import multicall from "./utils/multicall";
